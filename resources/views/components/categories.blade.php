eadd67276e7f1b02db89841df7a8a724a73e9ec2
Norayr Petrosyan <nordeveloper@gmail.com>
2022-11-07 22:40:09 +0400

    -

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,4 +1,4 @@
-@if (!$result->isEmpty())
+@if ( !empty($result) && !$result->isEmpty())
     <ul class="navbar-nav mr-auto">
         @foreach ($result as $item)
         <li class="nav-item"><a class="nav-link" href="/blog/catagory/{{$item->slug}}">{{$item->title}}</a></li>
